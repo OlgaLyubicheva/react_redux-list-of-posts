4bcf79490a7f1a16ed91ba76cfde46b2f69a4780
Olga Lyubicheva <forjobslov@gmail.com>
2019-08-18 23:15:02 +0300

    build

---
+++ build/precache-manifest.f151f8e53302b24b97088d2b82e2f160.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c7920b0d0a5f9b45d37c301b8e881a",
+    "url": "/react_redux-list-of-posts/index.html"
+  },
+  {
+    "revision": "1cf4b0c318cf10eb6bfb",
+    "url": "/react_redux-list-of-posts/static/css/main.24760afe.chunk.css"
+  },
+  {
+    "revision": "19ad4ff95b9fc458c577",
+    "url": "/react_redux-list-of-posts/static/js/2.a86c3f38.chunk.js"
+  },
+  {
+    "revision": "1cf4b0c318cf10eb6bfb",
+    "url": "/react_redux-list-of-posts/static/js/main.8e189ab2.chunk.js"
+  },
+  {
+    "revision": "c3092fe5bcf6cd34b5b5",
+    "url": "/react_redux-list-of-posts/static/js/runtime~main.d0e8944a.js"
+  }
+]);
